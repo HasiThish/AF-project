07b0827932b2c874544cfad137f6856ba6b922b4
HasiThish <86554418+HasiThish@users.noreply.github.com>
2023-04-25 21:47:27 +0530

    Created frontend and backend projects

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = (
+  <div>
+    <h1>Hello, world!</h1>
+    <p>This is my JSX code.</p>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
